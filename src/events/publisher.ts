1364d9168b02c6f318e866b4230d130c6a40aa98
Sanyam Jain <sanyamjaino98@gmail.com>
2021-06-11 12:14:39 +0530

    Awaiting event publication

--- src/events/publisher.ts
+++ src/events/publisher.ts
@@ -16,9 +16,15 @@ export abstract class Publisher<T extends Event> {
         this.client = client
     }
 
-    publish(data: T['data']) {
-        this.client.publish(this.subject, JSON.stringify(data), () => {
-            console.log('Published')
+    publish(data: T['data']): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.client.publish(this.subject, JSON.stringify(data), (err) => {
+                if (err) {
+                    return reject(err)
+                }
+
+                resolve()
+            })
         })
     }
 
--- src/publisher.ts
+++ src/publisher.ts
@@ -8,13 +8,13 @@ const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222',
 })
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS')
 
     const publisher = new TicketCreatedPublisher(stan)
 
     //enforces exact same object which we are passing
-    publisher.publish({
+    await publisher.publish({
         id: '123',
         title: 'concert',
         price: 20,
